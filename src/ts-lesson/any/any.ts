40a01a41383d1104894464da2d03012947995f0c
Yuom Kitya <kityayuom@gmail.com>
2025-10-27 22:36:38 +0700

    add inference

--- src/ts-lesson/any/any.ts
+++ src/ts-lesson/any/any.ts
@@ -2,3 +2,6 @@ let data: any = 5;
 data = "Hello ";
 data = true;
 console.log("Any:", data)
+
+
+//could be any type of data type but use any is not safe 
---
+++ src/ts-lesson/inference/inference.ts
@@ -0,0 +1,6 @@
+let country = "USA";
+let population = 331000000;
+let isDeveloped = true;
+console.log(country, population, isDeveloped)
+
+// it is clean and same safety as manual 
